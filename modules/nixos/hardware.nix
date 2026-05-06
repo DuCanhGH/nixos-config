bb643cda2ac2772c93180f70ff331be6a60cca3e
DuCanhGH <ngoducanh2912@gmail.com>
2026-05-06 07:31:32 -0400

    chore: update `boot.kernelParams`

--- modules/nixos/hardware.nix
+++ modules/nixos/hardware.nix
@@ -8,13 +8,10 @@
     # Enable "Silent boot"
     consoleLogLevel = 3;
     initrd.verbose = false;
-
     kernelParams = [
       "quiet"
-      "splash"
-      "boot.shell_on_fail"
       "udev.log_priority=3"
-      "rd.systemd.show_status=auto"
+      "systemd.show_status=auto"
     ];
   };
 
